4d6cc878eb8c0b3250b1af170ba88115c89aa7e2
WILLAN JOAQUIM <wilian@prongila.com>
2022-01-14 00:02:35 +0100

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+/* import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -6,4 +6,31 @@ function App() {
   );
 }
 
+export default App;*/
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from "pages/Listing/Form"; 
+import Navbar from "components/Navbar";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
+
---
+++ frontweb/src/pages/Listing/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form(){
+    return(
+        <h1>Página de Formulários</h1>
+    );
+}
+
+export default Form;
